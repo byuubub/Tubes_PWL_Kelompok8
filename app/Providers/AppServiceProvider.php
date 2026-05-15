5ab0c9c24e42efbe94bd32557466d274a21c7aaf
bayu <bayupranoto139@gmail.com>
2026-05-15 19:50:17 +0700

    feat: observer, middleware, queue logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models\MedicalRecord;      // <-- Tambahkan ini
-use App\Observers\MedicalRecordObserver; // <-- Pastikan observer sudah ada
+use App\Models\MedicalRecord;
+use App\Observers\MedicalRecordObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
